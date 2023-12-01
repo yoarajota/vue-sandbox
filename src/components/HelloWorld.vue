4eebf5d7b6270918b2dd6daad43df90f60105704
yoarajota <joaovbscontato@gmail.com>
2023-11-30 21:58:02 -0300

    Refactor button click event to update count and change background color

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,14 @@
 <script setup>
+const randomColors = ['#646cff', '#42b883', '#ff6e6e', '#ffcb6b']
+const currentColor = ref(randomColors[0])
+
 import { ref } from 'vue'
 
 defineProps({
   msg: String,
 })
+
+const element = ref(null)
 
 const count = ref(0)
 </script>
@@ -12,7 +17,9 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="() => {
+      count++; currentColor = randomColors[Math.floor(Math.random() * (5 - 0 + 1) + 0)]
+      }">count is {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
@@ -30,11 +37,8 @@ const count = ref(0)
     <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
     in your IDE for a better DX
   </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
-</template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
+  <p :ref="element" :style="{
+    '--var': currentColor,
+  }" class=" text-var">Click on the Vite and Vue logos to learn more</p>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'var': 'var(--var)',
+      }
+    },
   },
   plugins: [],
 }
