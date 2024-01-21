689cbb73020525664678e6a6a3307f598cc97cdd
yoarajota <joaovbscontato@gmail.com>
2024-01-20 23:40:08 -0300

    editroom start

---
+++ src/pages/EditRoom.vue
@@ -0,0 +1,20 @@
+<template>
+    <p contenteditable 
+    @keydown="italic"
+    @input="() => {}">
+        as
+    </p>
+</template>
+<script setup>
+
+
+// Function to if press cntrl I, in a contenteditable element, puts <i> tag arround string selected
+function italic(event) {
+    var selection = window.getSelection().toString();
+    var text = document.querySelector('p').innerHTML;
+    var newText = text.replace(selection, '<i>' + selection + '</i>');
+    document.querySelector('p').innerHTML = newText;
+}
+
+
+</script>
