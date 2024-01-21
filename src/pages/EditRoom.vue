749860d5eeae47b9e4ccf179e327cc39e38a1c64
yoarajota <joaovbscontato@gmail.com>
2024-01-21 14:57:43 -0300

    edit room!

--- src/pages/EditRoom.vue
+++ src/pages/EditRoom.vue
@@ -1,6 +1,6 @@
 <template>
     <p contenteditable @keydown="bold" @input="() => { }">
-        1laskdçla asçdlkasçld0oo 213sa
+       Texto mais legível!
     </p>
 </template>
 <script setup>
@@ -10,22 +10,33 @@ function bold(event) {
         event.preventDefault();
         const range = window.getSelection().getRangeAt(0);
         const parentElement = range.commonAncestorContainer.parentElement;
-
+        
         if (range.startOffset !== range.endOffset) {
+            const string = range.toString();
+
             if (parentElement.tagName === 'B') {
-                // If the selected text is already bold, remove the <b> tags
-                const newText = document.createTextNode(parentElement.textContent);
-                parentElement.replaceWith(newText);
-                newText.parentNode.normalize(); // Merge adjacent text nodes
+                if (parentElement.textContent !== string) {
+                    // If the selected text is only a part of the bold text, close the <b> tag before and start a new one after.
+                    const startText = parentElement.textContent.substring(0, range.startOffset);
+                    const selectedText = range.toString();
+                    const endText = parentElement.textContent.substring(range.endOffset);
+                    const newHTML = '<b>' + startText + '</b>' + selectedText + '<b>' + endText + '</b>';
+                    parentElement.outerHTML = newHTML;
+                } else {
+                    // If the selected text is already bold, remove the <b> tags
+                    const newText = document.createTextNode(parentElement.textContent);
+                    parentElement.replaceWith(newText);
+                    newText.parentNode.normalize(); // Merge adjacent text nodes
+                }
             } else {
                 // If the selected text is not bold, add the <b> tags
                 const boldNode = document.createElement('b');
-                boldNode.appendChild(document.createTextNode(range.toString()));
+                boldNode.appendChild(document.createTextNode(string));
                 range.deleteContents();
                 range.insertNode(boldNode);
             }
 
-            document.querySelectorAll('b').forEach(tag => {
+            parentElement.querySelectorAll('b').forEach(tag => {
                 if (tag.textContent.trim() === '') {
                     tag.remove();
                 }
